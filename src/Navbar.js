8aa1197c3895d59f7b382cab2ce5ec856bb8a2a9
maxxHildebrand <mbhildebrand6@gmail.com>
2024-01-27 23:03:41 -0500

    Up to 15

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>The Loco Blog</h1>
+            <div className="Links">
+                <a href="/">Home</a>
+                <a href="/create">New Blog</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
